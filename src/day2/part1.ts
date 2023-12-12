d9fcb72fde8170a12784721dd3126032b86b6b4a
Lee-XR <xian.rong1124@gmail.com>
2023-12-12 22:51:50 +0800

    feat(day2): add completed part 1 solution

--- src/day2/part1.ts
+++ src/day2/part1.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import { getAssetsDirectoryPath, readFileByLine } from '../utils/fs.ts';
+import { lineLogger, logger } from '../utils/log.ts';
 
 const colour = ['red', 'green', 'blue'] as const;
 type Colour = (typeof colour)[number];
@@ -16,26 +17,30 @@ const rl = readFileByLine(filePath);
 async function getSolution() {
 	const possibleGames: number[] = [];
 	for await (const line of rl) {
-		const [game, sets] = line.split(':');
+		const [game, sets] = line.split(': ');
 		const gameId: number = parseInt(game.replace(/[^0-9]/g, ''));
+		const highestCubeCount: Record<Colour, number> = {
+			red: 0,
+			green: 0,
+			blue: 0,
+		};
 
-		const cubesCount: Record<Colour, number> = { red: 0, green: 0, blue: 0 };
 		let isPossible = true;
-		for (const set of sets.split('; ')) {
+		setLoop: for (const set of sets.split('; ')) {
 			const cubes = set.split(', ');
 			for (const cube of cubes) {
 				const [stringNum, colour] = cube.split(' ') as [string, Colour];
 				const intNum = parseInt(stringNum);
-				if (cubesCount[colour] + intNum <= loadedCondition[colour]) {
-					cubesCount[colour] += intNum;
-				} else {
-					isPossible = false;
-					break;
-				}
-			}
-			if (!isPossible) break;
-		}
+				const currentHighestCube =
+					intNum > highestCubeCount[colour] ? intNum : highestCubeCount[colour];
 
+				if (currentHighestCube > loadedCondition[colour]) {
+					isPossible = false;
+					break setLoop;
+				}
+				highestCubeCount[colour] = currentHighestCube;
+			}
+		}
 		if (isPossible) possibleGames.push(gameId);
 	}
 
