c0e78f95b2e564d1fd836c18e41b9ff8c99f4f8e
Lee-XR <xian.rong1124@gmail.com>
2023-12-17 23:13:47 +0800

    feat(day-4): add part 1 solution

--- src/day4/part1.ts
+++ src/day4/part1.ts
@@ -1,12 +1,31 @@
 import { getInputFilePath, readFileByLine } from '../utils/fs.ts';
+import { log } from '../utils/log.ts';
 
 const filePath = getInputFilePath(4);
 const rl = readFileByLine(filePath);
 
 async function getSolution() {
+	let totalPoints = 0;
 	for await (const line of rl) {
+		let matches = 0;
+		const [_, numbers] = line.split(': ');
+		const [winningNums, numsList] = numbers
+			.split(' | ')
+			.map((nums) => nums.split(' ').filter((value) => value.length !== 0));
+
+		numsList.map((num) => {
+			if (winningNums.includes(num)) matches++;
+		});
+
+		if (matches === 1) totalPoints += 1;
+		if (matches > 1) {
+			totalPoints += (function fn(matches: number, val: number): number {
+				return matches > 1 ? fn(--matches, val * 2) : val;
+			})(matches, 1);
+		}
 	}
+	return totalPoints;
 }
 
-const Day4Part1Solution = getSolution();
+const Day4Part1Solution = await getSolution();
 export default Day4Part1Solution;
