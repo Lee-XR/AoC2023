e9bd6d59e8338597b207f5fe991de34f632493ea
Xian Rong Lee <xianrong@netizenexperience.com>
2023-12-18 09:09:49 +0800

    feat(day-4): add function to get number of matches for each line

--- src/day4/part2.ts
+++ src/day4/part2.ts
@@ -3,8 +3,17 @@ import { getInputFilePath, readFileByLine } from "../utils/fs.ts";
 const filePath = getInputFilePath(4);
 const rl = readFileByLine(filePath);
 
+function getMatches(line: string) {
+  const [_, numbers] = line.split(": ");
+  const [winningNums, numsList] = numbers
+    .split(" | ")
+    .map((nums) => nums.split(" ").filter((num) => num.length !== 0));
+  return numsList.filter((num) => winningNums.includes(num)).length;
+}
+
 async function getSolution() {
   for await (const line of rl) {
+    const matches = getMatches(line);
   }
 }
 
