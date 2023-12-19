80e338582a85055118a8fb4b53b2e874a8a08854
Lee-XR <xian.rong1124@gmail.com>
2023-12-19 22:22:52 +0800

    feat(utils): add function to get input file and test file path

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,17 +1,25 @@
-import { createReadStream } from "fs";
-import path from "path";
-import { createInterface } from "readline";
+import { createReadStream } from 'fs';
+import path from 'path';
+import { createInterface } from 'readline';
 
 export function getRootDirectoryPath() {
-  return path.join(process.cwd());
+	return path.join(process.cwd());
 }
 
 export function getAssetsDirectoryPath() {
-  return path.join(getRootDirectoryPath(), "src", "assets");
+	return path.join(getRootDirectoryPath(), 'src', 'assets');
+}
+
+export function getInputFilePath(day: number, fileName: string = 'input.txt') {
+	return path.join(getAssetsDirectoryPath(), `day${day}`, fileName);
+}
+
+export function getTestFilePath(day: number, fileName: string = 'test.txt') {
+	return path.join(getAssetsDirectoryPath(), `day${day}`, fileName);
 }
 
 export function readFileByLine(filePath: string) {
-  const fileStream = createReadStream(filePath);
-  const rl = createInterface({ input: fileStream });
-  return rl;
+	const fileStream = createReadStream(filePath);
+	const rl = createInterface({ input: fileStream });
+	return rl;
 }
