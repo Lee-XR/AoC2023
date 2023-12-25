e5a1f200ee2eb77c923e9498991541140f50a256
Lee-XR <xian.rong1124@gmail.com>
2023-12-25 17:58:34 +0800

    feat(day-5): add part 1 solution

--- src/day5/part1.ts
+++ src/day5/part1.ts
@@ -5,40 +5,65 @@ const filePath = getInputFilePath(5);
 const fileContentsArr: string[] = readFileSync(filePath, {
 	encoding: 'utf8',
 }).split('\n');
-const fileSectionsObj: Record<string, string[]> = {};
-let currentMap: string = '';
-fileContentsArr.map((line, index) => {
-	// First line set seeds value
-	if (index === 0) {
-		const [key, val] = line.split(': ');
-		fileSectionsObj[key] = [val];
-		return;
-	}
-	// Ignore empty character
-	if (line === '') return;
-	// Set map name as key
-	if (line.includes(':')) {
-		fileSectionsObj[line] = fileSectionsObj[line] ?? [];
-		currentMap = line;
-		return;
-	}
-	// Set map values into array
-	fileSectionsObj[currentMap] = [...fileSectionsObj[currentMap], line];
-	return;
-});
 
-function getMapValues(index: number) {
-	return Object.values(fileSectionsObj)[index];
+function getMapValues() {
+	let mapName = '';
+	const valuePairs = fileContentsArr.reduce<Record<string, string[]>>(
+		(acc, cur) => {
+			if (cur.includes(':')) {
+				mapName = cur.replace(':', '');
+				acc[mapName] = [];
+				return acc;
+			}
+			if (cur !== '') {
+				acc[mapName] = [...acc[mapName], cur];
+				return acc;
+			}
+			return acc;
+		},
+		{}
+	);
+	return {
+		seedValues: Object.values(valuePairs)[0],
+		seedToSoil: Object.values(valuePairs)[1],
+		soilToFertilizer: Object.values(valuePairs)[2],
+		fertilizerToWater: Object.values(valuePairs)[3],
+		waterToLight: Object.values(valuePairs)[4],
+		lightToTemperature: Object.values(valuePairs)[5],
+		temperatureToHumidity: Object.values(valuePairs)[6],
+		humidityToLocation: Object.values(valuePairs)[7],
+	};
 }
 
-const seeds = getMapValues(0);
-const seedToSoilMap = getMapValues(1);
-const soilToFertMap = getMapValues(2);
-const fertToWaterMap = getMapValues(3);
-const waterToLightMap = getMapValues(4);
-const lightToTempMap = getMapValues(5);
-const tempToHumidMap = getMapValues(6);
-const humidToLocationMap = getMapValues(7);
+function getMap(s: string) {
+	const v = s.split(' ');
+	return {
+		source: parseInt(v[1]),
+		destination: parseInt(v[0]),
+		range: parseInt(v[2]),
+	};
+}
 
-const Day5Part1Solution = Object.keys(fileSectionsObj).length;
+async function getSolution() {
+	const { seedValues, ...mapValues } = getMapValues();
+	const seedLocationValues: number[] = [];
+
+	seedValues[0].split(' ').forEach((val) => {
+		const intVal = parseInt(val);
+		let correspondingVal = intVal;
+		Object.values(mapValues).forEach((values) => {
+			for (let i = 0; i < values.length; i++) {
+				const { source, destination, range } = getMap(values[i]);
+				if (correspondingVal >= source && correspondingVal < source + range) {
+					correspondingVal += destination - source;
+					break;
+				}
+			}
+		});
+		seedLocationValues.push(correspondingVal);
+	});
+	return Math.min(...seedLocationValues);
+}
+
+const Day5Part1Solution = await getSolution();
 export default Day5Part1Solution;
